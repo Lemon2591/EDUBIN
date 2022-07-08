d084a13ccb700103cd3aa568c7caab223bf612cd
Lemon2591 <89698400+Lemon2591@users.noreply.github.com>
2022-07-09 01:34:04 +0700

    fix

--- js/app.js
+++ js/app.js
@@ -175,7 +175,7 @@ function renderData(dataResult) {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
