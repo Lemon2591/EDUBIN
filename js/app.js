ad706c9dd8a694f48711a10f2706188d7c6146fc
Lemon2591 <89698400+Lemon2591@users.noreply.github.com>
2022-07-09 09:47:46 +0700

    fix

--- js/app.js
+++ js/app.js
@@ -296,3 +296,42 @@ btnPlay.addEventListener("click", () => {
 video.addEventListener("click", () => {
   video.classList.remove("show-video");
 });
+
+$(document).ready(function () {
+  $(".autoplay4").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
